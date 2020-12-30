8eb413ba7c36956e736471e760a242d4e4d9e0ea
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-29 19:09:02 -0700

    Create instance variables for public and private repos to pass to view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class DashboardController < ApplicationController
   def show
+    @public_repos = RepositoryFacade.get_public_repos(current_user)
+    @private_repos = RepositoryFacade.get_private_repos(current_user)
   end
 end
